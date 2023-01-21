22df93f715f3e433e62dc76c2dc151d92523e231
Bill Brown <bill.a.brown90@gmail.com>
2023-01-21 14:46:40 -0800

    commit tree 13e1812709fc52e3e79e97d0b290672f29128bf3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+//First function
+function updateEmployeeWithKeyAndValue (obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+
+    return newObj
+}
+
+//second function
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value;
+    return obj
+}
+
+//third function for assignment
+function deleteFromEmployeeByKey (obj, key) {
+    const newObj = { ...obj }
+    delete newObj[key]
+
+    return newObj
+}
+
+//fourth function for assignment
+function destructivelyDeleteFromEmployeeByKey (obj, key) {
+    delete obj[key]
+    return obj
+}
