3a60e3adea2ada45d34d416dbdd311f52e99587c
Bill Brown <bill.a.brown90@gmail.com>
2023-02-25 10:13:23 -0800

    Basic Object manipulation

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+//object should log emplyee information:
+    //name
+    //address
+
+const employee = {
+    name:"Bilbo" ,
+    streetAddress: "111 Shire Lane",
+};
+
+
+//create the following functions for the lab:
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    const updatedEmployee = {...employeeObject};
+    updatedEmployee[key] = value;
+    return updatedEmployee
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value
+    return employeeObject
+};
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    const updatedEmployee = {...employeeObject}
+    delete updatedEmployee[key]
+    return updatedEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key];
+    return employeeObject;
+};
